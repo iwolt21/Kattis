690ec4c16a4f93f4cb11488c3205082f114dbb6d
iwolt21 <isaac_wolters@hotmail.com>
2022-01-07 17:05:36 -0400

    testing

--- Sum_Kind_of_Problem.py
+++ Sum_Kind_of_Problem.py
@@ -11,3 +11,4 @@ for i in range(1, sets+1):
     inputs[inp_list[0]][2] = int(inp_list[1])**2 + int(inp_list[1])
 for key in inputs:
     print("{} {} {} {}".format(key, inputs[key][0], inputs[key][1], inputs[key][2]))
+print("This is a git test")
